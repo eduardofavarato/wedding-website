dec77fabcc53ee88dca9ab62059c09a0aa077376
Eduardo Favarato <efavarato@gofundme.com>
2025-01-23 12:46:58 -0300

    Fix navbar behavior

--- js/scripts.js
+++ js/scripts.js
@@ -83,6 +83,28 @@ $(document).ready(function () {
 
     });
 
+    /***************** Header BG Scroll ******************/
+
+    $(function () {
+        $(window).scroll(function () {
+            var scroll = $(window).scrollTop();
+
+            if (scroll >= 20) {
+                $('section.navigation').addClass('fixed');
+                $('header').css({
+                    "border-bottom": "none",
+                    "padding": "35px 0"
+                });
+            } else {
+                $('section.navigation').removeClass('fixed');
+                $('header').css({
+                    "border-bottom": "solid 1px rgba(255, 255, 255, 0.2)",
+                    "padding": "35px 0"
+                });
+            }
+        });
+    });
+
     /***************** Smooth Scrolling ******************/
 
     $(function () {
